272d318e6b695d7222b4d937342cbfe89ef5d118
aouaha2s <ayman.ouahabi@smail.h-brs.de>
2023-04-08 00:04:06 +0200

    Update

---
+++ Krypto1/main.py
@@ -0,0 +1,32 @@
+char_list = [chr(char) for char in range(ord('A'), ord('Z')+1)]
+with open('chiffrat.txt', 'r') as f:
+    chiffrat = list(f.read())
+
+# Absolute Häufigkeit
+def calculate_a_frequency():
+    frequency = {}
+    for charToCompare in chiffrat:
+        if charToCompare in char_list:
+            if charToCompare in frequency:
+                frequency[charToCompare] += 1
+            else:
+                frequency[charToCompare] = 1
+        else:
+            continue
+    return frequency
+
+# Relative Häufigkeit
+def calculate_r_frequency(mylist):
+    total = sum(mylist.values())
+    frequency = {}
+    for char in mylist:
+        frequency[char] = round((mylist[char] / total) * 100, 2)
+    return frequency
+
+
+list = calculate_a_frequency()
+print(list)
+print(calculate_r_frequency(list))
+
+
+
